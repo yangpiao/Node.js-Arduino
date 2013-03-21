ef6c5cd1060a68fd5c659d9abbff74d7623466fa
Yang Piao <yp@yangpiao.net>
2013-03-21 18:43:16 -0400

    Added router for devices

---
+++ routes/device.js
@@ -0,0 +1,16 @@
+var Device = require('../models/device');
+
+exports.list = function(req, res) {
+};
+
+exports.show = function(req, res) {
+};
+
+exports.create = function(req, res) {
+};
+
+exports.update = function(req, res) {
+};
+
+exports.remove = function(req, res) {
+};
